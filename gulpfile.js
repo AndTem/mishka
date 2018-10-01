51bb2fac4fd401a81915b809b1d825e18dbd0bb7
Andrew Potemkin <ipad_nw@icloud.com>
2018-10-01 23:24:50 +0300

    для галпа добавлен htmlbeautify plugin. Сделана разметка для всех основных страниц.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 const gulp = require('gulp');
 const twig = require('gulp-twig');
-const watch = require('gulp-watch');
+const htmlbeautify = require('gulp-html-beautify');
+
+const htmlbeautifyOptions = {
+  indent_size: 2
+};
 
 gulp.task('compile-main-template', () => gulp.src('./source/pages/index.html')
   .pipe(twig())
+  .pipe(htmlbeautify(htmlbeautifyOptions))
   .pipe(gulp.dest('./source'))
 );
 
 gulp.task('compile-catalog-template', () => gulp.src('./source/pages/catalog.html')
   .pipe(twig())
+  .pipe(htmlbeautify(htmlbeautifyOptions))
   .pipe(gulp.dest('./source'))
 );
 
 gulp.task('compile-form-template', () => gulp.src('./source/pages/form.html')
   .pipe(twig())
+  .pipe(htmlbeautify(htmlbeautifyOptions))
   .pipe(gulp.dest('./source'))
 );
 
