a7a5f6a4417825fdd02825d39d45b67f34110764
Keks <keks@htmlacademy.ru>
2018-10-03 16:48:47 +0300

    :kissing_cat: Добавляет сборку Less на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,34 @@
-const gulp = require('gulp');
-const twig = require('gulp-twig');
-const htmlbeautify = require('gulp-html-beautify');
+"use strict";
 
-const htmlbeautifyOptions = {
-  indent_size: 2
-};
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 
-gulp.task('compile-main-template', () => gulp.src('./source/pages/index.html')
-  .pipe(twig())
-  .pipe(htmlbeautify(htmlbeautifyOptions))
-  .pipe(gulp.dest('./source'))
-);
-
-gulp.task('compile-catalog-template', () => gulp.src('./source/pages/catalog.html')
-  .pipe(twig())
-  .pipe(htmlbeautify(htmlbeautifyOptions))
-  .pipe(gulp.dest('./source'))
-);
-
-gulp.task('compile-form-template', () => gulp.src('./source/pages/form.html')
-  .pipe(twig())
-  .pipe(htmlbeautify(htmlbeautifyOptions))
-  .pipe(gulp.dest('./source'))
-);
-
-gulp.task('watch-templates', () => {
-  gulp.watch('./source/**/*.html', [
-    'compile-main-template',
-    'compile-catalog-template',
-    'compile-form-template'
-  ]);
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
+
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("start", gulp.series("css", "server"));
